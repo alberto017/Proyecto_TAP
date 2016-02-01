f73909514fb1c4027d2d0905caa0d9b87a752fe0
alberto017 <xavihdz09@gmail.com>
2016-01-31 20:35:16 -0600

    Contenedor del programa

---
+++ FlowLayout_2.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class FlowLayout_2 extends JFrame{
+  
+  public void FlowLayout(){
+  setTitle("FlowLayout_2");
+  setSize(700,600);
+  setDefaultCloseOperation(EXIT_ON_CLOSE);
+  setLocationRelativeTo(null);
+  setVisible(true);
+  }//Constructor
+
+  public static void main(String args[]){
+  FlowLayout_2 FL = new FlowLayout_2();
+  }//main
+
+}//class
