15dd6f6bc80c47b2a943e7911eaab928593a429d
alberto017 <xavihdz09@gmail.com>
2016-01-31 18:50:19 -0600

    Agregacion de librerias

--- FlowLayout_1.java
+++ FlowLayout_1.java
@@ -1,3 +1,13 @@
+ 
+ //Bibliotecas
+ import java.awt.FlowLayout;
+ import javax.swing.JButton;
+ import javax.swing.JCheckBox;
+ import javax.swing.JFrame;
+ import javax.swing.JLabel;
+ import javax.swing.JTextField;
+ import javax.swing.WindowsConstants;
+
 public class FlowLayout_1{
  
  public static void main(String args[]){
