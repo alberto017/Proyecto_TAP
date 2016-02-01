71fdc2a42a1ee7d3edd5f4c5f8b0388e1e3d1ff0
alberto017 <xavihdz09@gmail.com>
2016-01-31 21:11:06 -0600

    Agregar los comentarios a nuestro programa

--- FlowLayout_2.java
+++ FlowLayout_2.java
@@ -1,22 +1,24 @@
+ //Bibliotecas
+ import javax.swing.JFrame;
+ import java.awt.FlowLayout;
+ import javax.swing.JButton;
 
-import javax.swing.JFrame;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-
-public class FlowLayout_2 extends JFrame{
-
-  JButton boton_1 = new JButton("Boton 1");
-  JButton boton_2 = new JButton("Boton 2");
-  JButton boton_3 = new JButton("Boton 3");
+ public class FlowLayout_2 extends JFrame{
+   //Creacion de componentes fuera del constructor
+   JButton boton_1 = new JButton("Boton 1");
+   JButton boton_2 = new JButton("Boton 2");
+   JButton boton_3 = new JButton("Boton 3");
   
    public void FlowLayout_2(){
-
+     //Creacion y asignacion del Layout a nustro JFrame
       setLayout(new FlowLayout(FlowLayout.LEFT));
-  
+   
+      //Agregar componentes
       add(boton_1);
       add(boton_2);
       add(boton_3);
   
+      //Asignar las propiedades de nuestro JFrame(ventana)
        setTitle("FlowLayout_2");
        setSize(700,600);
        setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -27,5 +29,4 @@ public class FlowLayout_2 extends JFrame{
     public static void main(String args[]){
     FlowLayout_2 FL = new FlowLayout_2();
   }//main
-
 }//class
