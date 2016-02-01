99adf052d9ae17d4d80515b1e47c712145495876
alberto017 <xavihdz09@gmail.com>
2016-01-31 21:02:50 -0600

    Agregar los comentarios a nuestro programa

--- FlowLayout_1.java
+++ FlowLayout_1.java
@@ -11,22 +11,27 @@
 public class FlowLayout_1{
  
  public static void main(String args[]){
- 
-    JFrame v = new JFrame();  
+  
+    //Crear nuestro JFrame(ventana)
+    JFrame v = new JFrame();
+   
+    //Asignar el Layout a nuestro JFrame(ventana)  
     v.getContentPane().setLayout(new FlowLayout());
 
+    //Crear los componentes graficos
     JButton boton = new JButton("Boton");
     JTextField campo = new JTextField(10);
     JCheckBox check = new JCheckBox("CheckBox");		
     JLabel etiqueta = new JLabel("Etiqueta");
     
+    //Agregar los componentes al JFame(Ventana)
     v.getContentPane().add(boton);
     v.getContentPane().add(campo);	
     v.getContentPane().add(check);	
     v.getContentPane().add(etiqueta);
     
+   //Asignar las propiedades al JFrame(Ventana)
     v.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
     v.pack();	
     v.setVisible(true);
 
