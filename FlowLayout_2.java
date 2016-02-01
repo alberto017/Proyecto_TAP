b7850a4a5c9b803f90b5e9a30ed8a68182f8f544
alberto017 <xavihdz09@gmail.com>
2016-01-31 20:57:39 -0600

    Agregacion de componentes

--- FlowLayout_2.java
+++ FlowLayout_2.java
@@ -1,17 +1,31 @@
+
 import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 public class FlowLayout_2 extends JFrame{
-  
-  public void FlowLayout(){
-  setTitle("FlowLayout_2");
-  setSize(700,600);
-  setDefaultCloseOperation(EXIT_ON_CLOSE);
-  setLocationRelativeTo(null);
-  setVisible(true);
-  }//Constructor
 
-  public static void main(String args[]){
-  FlowLayout_2 FL = new FlowLayout_2();
+  JButton boton_1 = new JButton("Boton 1");
+  JButton boton_2 = new JButton("Boton 2");
+  JButton boton_3 = new JButton("Boton 3");
+  
+   public void FlowLayout_2(){
+
+      setLayout(new FlowLayout(FlowLayout.LEFT));
+  
+      add(boton_1);
+      add(boton_2);
+      add(boton_3);
+  
+       setTitle("FlowLayout_2");
+       setSize(700,600);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setLocationRelativeTo(null);
+       setVisible(true);
+     }//Constructor
+
+    public static void main(String args[]){
+    FlowLayout_2 FL = new FlowLayout_2();
   }//main
 
 }//class
