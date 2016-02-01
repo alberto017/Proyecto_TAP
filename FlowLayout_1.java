aa7b18f7e862abc6119412f4b9f3064b68ee07b8
alberto017 <xavihdz09@gmail.com>
2016-01-31 19:06:54 -0600

    Agregacion de componentes

--- FlowLayout_1.java
+++ FlowLayout_1.java
@@ -6,10 +6,30 @@
  import javax.swing.JFrame;
  import javax.swing.JLabel;
  import javax.swing.JTextField;
- import javax.swing.WindowsConstants;
+ import javax.swing.WindowConstants;
 
 public class FlowLayout_1{
  
  public static void main(String args[]){
- }//main
+ 
+    JFrame v = new JFrame();  
+    v.getContentPane().setLayout(new FlowLayout());
+
+    JButton boton = new JButton("Boton");
+    JTextField campo = new JTextField(10);
+    JCheckBox check = new JCheckBox("CheckBox");		
+    JLabel etiqueta = new JLabel("Etiqueta");
+    
+    v.getContentPane().add(boton);
+    v.getContentPane().add(campo);	
+    v.getContentPane().add(check);	
+    v.getContentPane().add(etiqueta);
+    
+    v.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    v.pack();	
+    v.setVisible(true);
+
+    }//main
+
 }//class
